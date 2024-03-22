350b4cb2418fdfcb47638972dc4fa01a6975ee8f
ggoma <sungwoopark95@gmail.com>
2024-03-22 22:42:33 +0900

    move all async data to ipfs status

--- services/IpfsService.ts
+++ services/IpfsService.ts
@@ -1,27 +1,54 @@
 'use server';
+import { SdkSupportedChainIds, mintclub } from 'mint.club-v2-sdk';
+import fetch from 'node-fetch';
 import { prisma } from '../database/prisma';
 import { FILEBASE_API_KEY } from './server-env';
-import { FilebaseClient } from '@filebase/client';
-import fetch from 'node-fetch';
+import { generateNextAvailableSymbol } from '../utils/symbol';
+import { kebabCase } from 'lodash-es';
 
-const client = new FilebaseClient({ token: FILEBASE_API_KEY });
-
-export async function uploadToIpfs(url: string) {
+export async function uploadToIpfs(params: {
+  url: string;
+  collectionName: string;
+  chainId: SdkSupportedChainIds;
+}) {
+  const { chainId, url, collectionName } = params;
   const status = await prisma.ipfsStatus.create({ data: {} });
 
   async function upload() {
-    console.log('uploading to ipfs');
+    console.log('uploading to ipfs & other metadata');
     try {
-      const arrayBuffer = await (await fetch(url)).arrayBuffer();
-      const blob = new Blob([arrayBuffer]);
-      const cid = await client.storeBlob(blob);
+      const blob = await (await fetch(url)).blob();
+      const symbolFromName = kebabCase(collectionName);
+      const creationFee = await mintclub.network(chainId).bond.getCreationFee();
+
+      const { symbol } = await generateNextAvailableSymbol({
+        chainId,
+        symbol: symbolFromName,
+        tokenType: 'ERC1155',
+      });
+
+      const imageHash = await mintclub.ipfs.upload({
+        filebaseApiKey: FILEBASE_API_KEY,
+        media: blob,
+      });
+      console.log({ imageHash });
+
+      const metadataHash = await mintclub.ipfs.uploadMetadata({
+        filebaseApiKey: FILEBASE_API_KEY,
+        image: imageHash as `ipfs://${string}`,
+        name: collectionName,
+      });
+
+      console.log({ metadataHash });
       await prisma.ipfsStatus.update({
         where: {
           id: status.id,
         },
         data: {
           status: 'SUCCESS',
-          ipfsHash: cid,
+          ipfsHash: metadataHash,
+          nftSymbol: symbol,
+          creationFee: creationFee.toString(),
         },
       });
     } catch (e) {
