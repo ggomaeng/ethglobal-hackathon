a587a5c8f4d9dade6daddc4016b17a943db5db59
ggoma <sungwoopark95@gmail.com>
2024-03-22 19:35:43 +0900

    Add index.tsx file with initial code

---
+++ src/index.tsx
@@ -0,0 +1,91 @@
+/* eslint-disable react/jsx-key */
+/** @jsxImportSource frog/jsx */
+
+// frog documentation
+// https://frog.fm/platforms/next
+import { Frog } from 'frog';
+import { devtools } from 'frog/dev';
+import { serveStatic } from 'frog/serve-static';
+import { readFile } from 'fs/promises';
+import { Logger } from '../utils/Logger';
+import { startProxy } from '../utils/proxy';
+import { ASSETS_PATH, BASE_FRAMES_PATH } from './common/config';
+
+declare global {
+  var cloudflared: string | undefined;
+}
+
+if (process.env.PROXY === 'true' && !globalThis.cloudflared) {
+  const cloudflared = await startProxy();
+  globalThis.cloudflared = cloudflared;
+}
+
+const rootDir = process.cwd();
+
+async function getFonts() {
+  const Bebas = await readFile(`${rootDir}/fonts/BebasNeue-Regular.ttf`);
+  const Roboto = await readFile(`${rootDir}/fonts/Roboto-Regular.ttf`);
+
+  return [
+    {
+      name: 'bebas',
+      data: Bebas,
+      weight: 400,
+    } as const,
+    {
+      name: 'roboto',
+      data: Roboto,
+      weight: 400,
+    } as const,
+  ];
+}
+
+type RootState = {
+  error?: string;
+};
+
+export const app = new Frog<{
+  State: RootState;
+}>({
+  verify: 'silent',
+  assetsPath: ASSETS_PATH,
+  basePath: BASE_FRAMES_PATH,
+  origin:
+    process.env.PROXY && globalThis.cloudflared !== undefined
+      ? globalThis.cloudflared
+      : process.env.NODE_ENV === 'development'
+        ? `http://localhost:${process.env.PORT}`
+        : 'https://mint.club',
+  imageOptions: {
+    format: 'png',
+    height: 600,
+    width: 600,
+    // debug: true,
+    fonts: await getFonts(),
+  },
+});
+
+app.use(async (c, next) => {
+  Logger.info(`[${c.req.method}] ${new URL(c.req.url).pathname}`);
+  await next();
+});
+
+if (typeof Bun !== 'undefined') {
+  app.get(
+    '/assets/*',
+    (await import('hono/bun')).serveStatic({
+      root: './',
+      rewriteRequestPath: (path) => {
+        return path.replace(/^\/frames\/assets/, '/public/assets');
+      },
+    }),
+  );
+  const port = process.env.PORT || 8880;
+  Bun.serve({
+    fetch: app.fetch,
+    port,
+  });
+  console.log(`Server is running on http://localhost:${port}/frames`);
+} else {
+  devtools(app, { serveStatic });
+}
