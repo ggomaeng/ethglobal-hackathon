20609c89c61db1c6e1268e1604f79601a243f7ed
ggoma <sungwoopark95@gmail.com>
2024-03-22 19:45:57 +0900

    add colors constants 🎨

---
+++ constants/colors.ts
@@ -0,0 +1,13 @@
+const colors = {
+  primary: '#15e6b7',
+  bg: '#000',
+  error: '#ff0000',
+  twitter: '#08a0e9',
+  freemint: '#c6f120',
+  warpcast: '#794aee',
+  grey: '#848795',
+  'base-chain': '#0154FD',
+  white: '#fff',
+};
+
+export { colors };
